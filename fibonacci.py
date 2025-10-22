928f09f50a96b43615dd0dd604327dd049f2dd36
HaiderSyed786 <minhajhaider32@gmail.com>
2025-10-22 06:07:08 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,12 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+def getuserinput()
+  while true
+    t = int(input("Enter number of terms for Fibonacci sequence: ))
+    if  t <= 0:
+      print("Please enter postive integer")
+    else:
+    return t
+    
